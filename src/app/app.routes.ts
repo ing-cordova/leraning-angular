5061018d54b15bf81d553978c3195424382e0be4
Andres Cordova <andrescordovaoficial@gmail.com>
2024-04-24 21:06:58 -0600

    Initialazing Project...

---
+++ src/app/app.routes.ts
@@ -0,0 +1,55 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component'),
+        children: [
+            {
+                path: 'change-detection',
+                title: 'Change Detection',
+                loadComponent: () => import('./dashboard/pages/change-detection/change-detection.component'),
+            },
+            {
+                path: 'control-flow',
+                title: 'Control Flow',
+                loadComponent: () => import('./dashboard/pages/control-flow/control-flow.component'),
+            },
+            {
+                path: 'defer-options',
+                title: 'Defer Options',
+                loadComponent: () => import('./dashboard/pages/defer-options/defer-options.component'),
+            },
+            {
+                path: 'defer-views',
+                title: 'Defer Views',
+                loadComponent: () => import('./dashboard/pages/defer-views/defer-views.component'),
+            },
+            {
+                path: 'user/:id',
+                title: 'User View',
+                loadComponent: () => import('./dashboard/pages/user/user.component'),
+            },
+            {
+                path: 'user-list',
+                title: 'User List',
+                loadComponent: () => import('./dashboard/pages/users/users.component'),
+            },
+            {
+                path: 'view-transtions',
+                title: 'View Transitions',
+                loadComponent: () => import('./dashboard/pages/view-transtions/view-transtions.component'),
+            },
+            {
+                path: '',
+                redirectTo: 'control-flow',
+                pathMatch: 'full'
+            }
+        ]
+    }
+];
---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-users-loader',
+    standalone: true,
+    imports: [],
+    template: `<h1>Loading users...</h1>`
+})
+
+export class UsersLoaderComponent {
+    
+}
